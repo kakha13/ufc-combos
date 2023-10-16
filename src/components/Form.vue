f35bc93f4b860fbc3a220857dd4dc8c42b396233
KAKHA13 <kakhagiorgashvili@gmail.com>
2023-10-17 00:42:12 +0400

    pages

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
-
-defineProps({
-  msg: String,
-})
+import KeysList from './KeysList.vue'
 
 const keys = ref('["L2","L1","Square","-","L2","L1","Triangle","-","L2","L1","Square"]');
 const delay = ref(1);
 const name = ref('')
 const link = ref('')
+const open = ref(true)
 
+let keysArray = ["Triangle", "Circle", "Cross", "Square", "L1", "L2", "R2"];
 const divider = "-";
 
 // computed delay convert to ms
@@ -19,72 +18,49 @@ const delayMs = computed(() => {
 
 
 const displayElementsWithDelay = () => {
-
   let jsonArray = JSON.parse(keys.value);
-  let elementsBeforeDivider = [];
-  let elementsAfterDivider = [];
-  let showElements = elementsBeforeDivider;
+  let elementGroups = [[]];
   for (const element of jsonArray) {
     if (element === divider) {
-      showElements = elementsAfterDivider;
+      elementGroups.push([]);
       continue;
     }
-
-    showElements.push(element);
+    elementGroups[elementGroups.length - 1].push(element);
   }
 
-  // Display the first set of elements with the "pressed" class
-  elementsBeforeDivider.forEach(element => {
-    console.log(element);
-    add(element);
-  });
+  // Display each set of elements with the "pressed" class
+  let delay = 0;
+  for (const elements of elementGroups) {
+    setTimeout(() => {
+      elements.forEach(element => {
+        add(element);
+      });
+    }, delay);
 
-  // Wait for 1000ms and then remove the "pressed" class from the first set of elements
-  setTimeout(() => {
-    elementsBeforeDivider.forEach(element => {
-      rm(element);
-    });
-
-
-  }, delayMs.value);
-  setTimeout(() => {
-    // Display the second set of elements with the "pressed" class
-    elementsAfterDivider.forEach(element => {
-      console.log(element);
-      add(element);
-
-    });
-  }, delayMs.value * 1.5);
+    delay += delayMs.value;
+    setTimeout(() => {
+      elements.forEach(element => {
+        rm(element);
+      });
+    }, delay);
+  }
 
 }
 
 const add = (element) => {
-  if (["Triangle", "Circle", "Cross", "Square", "L1", "L2","R2"].includes(element)) {
-    let elementSelector = document.querySelector(`[data-key='${element}']`);
-    elementSelector.classList.add("pressed");
-  } 
+  let elementSelector = document.querySelector(`[data-key='${element}']`);
+  elementSelector.classList.add("pressed");
 }
 
 const rm = (element) => {
-  // remove all pressed class
-
   document.querySelectorAll(".pressed").forEach(element => {
     element.classList.remove("pressed")
   });
-
-  // // if element in array Triangle Circle Cross Square 
-  // if (["Triangle", "Circle", "Cross", "Square", "L1", "L2","R2"].includes(element)) {
-  //   let elementSelector = document.querySelector(`[data-key='${element}']`);
-  //   elementSelector.classList.remove("pressed");
-  // }
 }
 
 function setHashStr() {
   let hesh = JSON.parse(keys.value).map(item => item);
   hesh = btoa(`${hesh}|${delay.value}|${name.value}|${link.value}`);
-
-  console.log(hesh);
-
   location.hash = hesh
 }
 
@@ -114,26 +90,55 @@ function reverse(inputString) {
 const sendKeys = () => {
   console.log("Sending keys");
 
-  document.querySelectorAll(".pressed").forEach(element => {
-    element.classList.remove("pressed")
-  });
+  rm();
 
   displayElementsWithDelay()
 }
 
+
 onMounted(() => {
   if (location.hash) {
-    console.log(location.hash);
     keys.value = JSON.stringify(reverse(location.hash.slice(1)));
   }
 })
 
-const count = ref(0)
 </script>
 
 <template>
-  <form class="w-96 bg-gray-200 dark:bg-slate-700 p-2 absolute rounded m-2">
-    <div class="flex flex-col gap-2">
+  <form class=" bg-gray-200 dark:bg-slate-700 p-2 absolute rounded m-2">
+
+    <div class="flex gap-3">
+      <button data-collapse-toggle="navbar-hamburger" type="button" @click="open = !open"
+        class="inline-flex items-center mr-auto outline-none justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-hamburger" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+      <button type="button" @click="sendKeys()"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+
+        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459" fill="currentColor"
+          xml:space="preserve">
+          <path
+            d="M229.5 0C102.751 0 0 102.751 0 229.5S102.751 459 229.5 459 459 356.249 459 229.5 356.249 0 229.5 0zm80.792 239.651-111.764 76.084a12.281 12.281 0 0 1-19.19-10.151V153.416a12.281 12.281 0 0 1 19.19-10.151l111.764 76.084a12.28 12.28 0 0 1 0 20.302z" />
+        </svg>
+        <span class="sr-only">Send</span>
+      </button>
+      <button type="button" @click="setHashStr()"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <!-- share svg -->
+        <svg class="w-5 h-5" version="1.1" fill="currentColor" viewBox="0 0 458.62 458.62" xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m339.59 314.53c-14.215 0-27.456 4.133-38.621 11.239l-112.68-78.67c1.809-6.315 2.798-12.976 2.798-19.871 0-6.896-0.989-13.557-2.798-19.871l109.64-76.547c11.764 8.356 26.133 13.286 41.662 13.286 39.79 0 72.047-32.257 72.047-72.047s-32.256-72.048-72.046-72.048-72.047 32.257-72.047 72.047c0 5.255 0.578 10.373 1.646 15.308l-112.42 78.491c-10.974-6.759-23.892-10.666-37.727-10.666-39.79 0-72.047 32.257-72.047 72.047s32.256 72.047 72.047 72.047c13.834 0 26.753-3.907 37.727-10.666l113.29 79.097c-1.629 6.017-2.514 12.34-2.514 18.872 0 39.79 32.257 72.047 72.047 72.047s72.047-32.257 72.047-72.047-32.257-72.048-72.047-72.048z" />
+        </svg>
+        <span class="sr-only">Share</span>
+      </button>
+    </div>
+    <div class="flex flex-col gap-2 w-96" :class="open ? '' : 'hidden'">
 
       <div>
         <h1 class="text-xl heading-normal font-bold text-gray-900 dark:text-white">{{ name }}</h1>
@@ -141,6 +146,14 @@ const count = ref(0)
         <div class="aspect-w-16 aspect-h-9">
           <img :src="link" alt="">
         </div>
+      </div>
+      <div>
+        <!-- show keys as tags -->
+        <div class="flex flex-wrap gap-1">
+          <KeysList :keys="keys"></KeysList>
+
+        </div>
+
       </div>
       <div>
         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -179,28 +192,7 @@ const count = ref(0)
 
 
         </div>
-        <div>
-          <button type="button" @click="sendKeys()"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 
-            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459" fill="currentColor"
-              xml:space="preserve">
-              <path
-                d="M229.5 0C102.751 0 0 102.751 0 229.5S102.751 459 229.5 459 459 356.249 459 229.5 356.249 0 229.5 0zm80.792 239.651-111.764 76.084a12.281 12.281 0 0 1-19.19-10.151V153.416a12.281 12.281 0 0 1 19.19-10.151l111.764 76.084a12.28 12.28 0 0 1 0 20.302z" />
-            </svg>
-            <span class="sr-only">Send</span>
-          </button>
-          <button type="button" @click="setHashStr()"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <!-- share svg -->
-            <svg class="w-5 h-5" version="1.1" fill="currentColor" viewBox="0 0 458.62 458.62" xml:space="preserve"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="m339.59 314.53c-14.215 0-27.456 4.133-38.621 11.239l-112.68-78.67c1.809-6.315 2.798-12.976 2.798-19.871 0-6.896-0.989-13.557-2.798-19.871l109.64-76.547c11.764 8.356 26.133 13.286 41.662 13.286 39.79 0 72.047-32.257 72.047-72.047s-32.256-72.048-72.046-72.048-72.047 32.257-72.047 72.047c0 5.255 0.578 10.373 1.646 15.308l-112.42 78.491c-10.974-6.759-23.892-10.666-37.727-10.666-39.79 0-72.047 32.257-72.047 72.047s32.256 72.047 72.047 72.047c13.834 0 26.753-3.907 37.727-10.666l113.29 79.097c-1.629 6.017-2.514 12.34-2.514 18.872 0 39.79 32.257 72.047 72.047 72.047s72.047-32.257 72.047-72.047-32.257-72.048-72.047-72.048z" />
-            </svg>
-            <span class="sr-only">Share</span>
-          </button>
-        </div>
       </div>
 
     </div>
---
+++ src/components/KeysList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex flex-wrap gap-1 justify-start ">
+        <span v-for="key in JSON.parse(keys)" :key="key" :class="key == '-' ? 'tag-' : 'tag'">
+            {{ renderKey(key) }}
+            <button type="button"
+                class="inline-flex items-center p-1 text-sm text-green-400 bg-transparent rounded-sm hover:bg-green-200 hover:text-green-900 dark:hover:bg-green-800 dark:hover:text-green-300"
+                data-dismiss-target="#badge-dismiss-default" aria-label="Remove">
+                <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Remove badge</span>
+            </button>
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, onMounted } from "vue";
+
+const props = defineProps({
+    keys: Object | String,
+})
+
+onMounted(() => {
+    console.log(props.keys);
+})
+
+const renderKey = (key) => {
+    if (key === "-") return `⏱️`
+    return key;
+}
+
+</script>
